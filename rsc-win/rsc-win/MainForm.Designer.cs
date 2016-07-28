3165a223686855ee6be6c8fc9ed09f67fd6af5b6
zhoudd <zdd_910@126.com>
2016-07-28 23:12:47 +0800

    inti

---
+++ rsc-win/rsc-win/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： zhoudd
+ * 日期: 2016/7/28
+ * 时间: 22:02
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+namespace rsc_win
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.LinkLabel linkLabel3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(270, 85);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 58);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "日升昌记";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// linkLabel3
+			// 
+			this.linkLabel3.Location = new System.Drawing.Point(6, 209);
+			this.linkLabel3.Name = "linkLabel3";
+			this.linkLabel3.Size = new System.Drawing.Size(600, 23);
+			this.linkLabel3.TabIndex = 1;
+			this.linkLabel3.TabStop = true;
+			this.linkLabel3.Text = "linkLabel1";
+			this.linkLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(673, 347);
+			this.Controls.Add(this.linkLabel3);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "rsc-win";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ rsc-win/rsc-win/MainForm.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： zhoudd
+ * 日期: 2016/7/28
+ * 时间: 22:02
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace rsc_win
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			linkLabel3.Text = "csdn Win 百度 谷歌 YAHOO";  
+			linkLabel3.Links.Add(0, 4, "http://www.csdn.net/");  
+			linkLabel3.Links.Add(9, 2, "http://www.baidu.com/");  
+			linkLabel3.Links.Add(12, 2, "http://www.google.cn/");  
+			linkLabel3.Links.Add(15, 5, "http://www.yahoo.com.cn/"); 
+//			System.Diagnostics.Process.Start("http://blog.csdn.net/testcs_dn"); 
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+//			linkLabel1.Links
+		}
+		void LinkLabel3LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			linkLabel3.Links[linkLabel3.Links.IndexOf(e.Link)].Visited = true;  
+			string targetUrl = e.Link.LinkData as string;  
+			if (string.IsNullOrEmpty(targetUrl))
+				MessageBox.Show("没有链接地址！");
+			else
+				System.Diagnostics.Process.Start(targetUrl);  
+		}
+	}
+}
